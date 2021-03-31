0bee6f41e3742063ac1b8da5aa0ac5390fe91d7b
Gabriel Sampaio <gasampaio.souza@gmail.com>
2021-03-31 17:15:30 -0300

    refactoring getPokemonColors
    i changed it from switch case to a single object, is it better?

--- src/utils/getPokemonTypeColor.ts
+++ src/utils/getPokemonTypeColor.ts
@@ -1,44 +1,27 @@
 import {TPokemon} from '../typescript/types/TPokemon';
 
 const getPokemonTypeColor = (type: TPokemon) => {
-  switch (type) {
-    case 'fire':
-      return '#FA5543';
-    case 'water':
-      return '#6890F0';
-    case 'grass':
-      return '#8BD650';
-    case 'eletric':
-      return '#F8D030';
-    case 'ice':
-      return '#98D8D8';
-    case 'ground':
-      return '#E0C068';
-    case 'flying':
-      return '#A890F0';
-    case 'poison':
-      return '#A040A0';
-    case 'fighting':
-      return '#C03028';
-    case 'psychic':
-      return '#F85888';
-    case 'dark':
-      return '#705848';
-    case 'rock':
-      return '#B8A038';
-    case 'bug':
-      return '#A8B820';
-    case 'ghost':
-      return '#705898';
-    case 'steel':
-      return '#B8B8D0';
-    case 'dragon':
-      return '#7038F8';
-    case 'fairy':
-      return '#FFAEC9';
-    default:
-      return '#A8A878';
+  const pokemonColors = {
+    fire: '#FA5543',
+    water: '#6890F0',
+    grass: '#8BD650',
+    eletric: '#F8D030',
+    ice: '#98D8D8',
+    ground: '#E0C068',
+    flying: '#A890F0',
+    poison: '#A040A0',
+    fighting: '#C03028',
+    psychic: '#F85888',
+    dark: '#705848',
+    rock: '#B8A038',
+    bug: '#A8B820',
+    ghost: '#705898',
+    steel: '#B8B8D0',
+    dragon: '#7038F8',
+    fairy: '#FFAEC9',
   }
+
+  return pokemonColors[type] || '#A8A878';
 };
 
 export default getPokemonTypeColor;
